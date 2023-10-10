6f596e2a0296017cf2c18b8eb44b2675a3e5a1d2
pharanyuwave@gmail.com <pharanyuwave@gmail.com>
2023-10-10 20:31:13 +0700

    edit useNavigate

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,9 +3,8 @@ import { Link, useNavigate } from 'react-router-dom'
 import { getUser, logout } from '../services/authorize'
 
 const Header = () => {
-    const [date, setDate] = useState(new Date())
-    
     const navigate = useNavigate()
+    const [date, setDate] = useState(new Date())
 
     const style = { height : "70px", marginBottom: "5px" }
 
